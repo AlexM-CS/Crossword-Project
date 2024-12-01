f34f875268ba20ba7d40ca0eef8575bba84076c4
bknautz <brandon.knautz@gmail.com>
2024-12-01 17:02:09 -0600

    more prompt changes

--- OpenAITest/InputedPrompt.py
+++ OpenAITest/InputedPrompt.py
@@ -7,7 +7,7 @@ def generate_Prompt(topic):
     prompt = """Generate a crossword hint for following separated by commas
      {topic}
      , making each simple, yet concise. 
-     Keep each hint to a single line, with only the hints being returned. Separate each hint by the word
-     "___". After finding a hint for the last word, do not add a "___"
+     Keep each hint to on the same line, with only the the hints being returned. Separate each hint by the letter
+     "Ɇ". After finding a hint for the last word, do not add a "Ɇ"
      Do not include number of letters in the word.""".format(topic=topic)
     return prompt
